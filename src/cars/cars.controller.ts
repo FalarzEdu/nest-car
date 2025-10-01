792a3a1db40e5270f171e687ab7dd45849eaf311
Eduardo Falarz <eduardofalarz23@gmail.com>
2025-10-01 02:25:32 -0300

    FEAT: Adding role based authorization

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -17,7 +17,9 @@ import { CreateCarDto } from "./dto/create-car-dto";
 import { QueryFilterDto } from "./dto/query-filter-dto";
 import { ResponseInterceptor } from "../response/response.interceptor";
 import { CustomExceptionFilter } from "../custom-exception/custom-exception.filter";
-import {JwtAuthGuard} from "../auth/jwt-auth-guard";
+import { JwtAuthGuard } from "../auth/jwt-auth-guard";
+import { RolesGuard } from "../auth/roles.guard";
+import { Roles } from "../auth/roles.decorator";
 
 @Controller("cars")
 @UseInterceptors(ResponseInterceptor)
@@ -26,6 +28,7 @@ import {JwtAuthGuard} from "../auth/jwt-auth-guard";
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
+  @Roles("admin")
   @Post()
   @HttpCode(201)
   create(@Body() createCar: CreateCarDto) {
@@ -44,18 +47,21 @@ export class CarsController {
     return this.carsService.findOne(+id);
   }
 
+  @Roles("admin")
   @Put(":id")
   @HttpCode(200)
   update(@Param("id") id: number, @Body() updateItem: any) {
     return this.carsService.update(id, updateItem);
   }
 
+  @Roles("admin")
   @Patch(":id")
   @HttpCode(200)
   partialUpdate(@Param("id") id: number, @Body() updateItem: any) {
     return this.carsService.update(id, updateItem);
   }
 
+  @Roles("admin")
   @Delete(":id")
   @HttpCode(204)
   delete(@Param("id") id: number) {

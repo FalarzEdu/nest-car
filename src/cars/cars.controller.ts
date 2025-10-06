929200ca63f6abae4c064eff14072112aa39933e
Eduardo Falarz <eduardofalarz23@gmail.com>
2025-10-06 18:47:14 -0300

    DOCS: Adding more decorators

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -9,7 +9,8 @@ import {
   Post,
   Put,
   Query,
-  UseFilters, UseGuards,
+  UseFilters,
+  UseGuards,
   UseInterceptors,
 } from "@nestjs/common";
 import { CarsService } from "./cars.service";
@@ -21,7 +22,8 @@ import { JwtAuthGuard } from "../auth/jwt-auth-guard";
 import { RolesGuard } from "../auth/roles.guard";
 import { Roles } from "../auth/roles.decorator";
 import {
-  ApiBearerAuth, ApiBody,
+  ApiBearerAuth,
+  ApiBody,
   ApiOperation,
   ApiResponse,
   ApiTags,
@@ -36,14 +38,81 @@ import {
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
-  @ApiOperation({ summary: "Cria um carro no banco de dados." })
-  @ApiBody({ type: CreateCarDto })
-  @ApiResponse({ status: 200, description: "Carro criado." })
-  @ApiResponse({ status: 403, description: "Não autorizado." })
-  @ApiResponse({ status: 422, description: "Dados inválidos." })
   @Roles("admin")
   @Post()
   @HttpCode(201)
+  // Documentação da Operação
+  @ApiOperation({
+    summary: "Criar um novo registro de carro",
+    description:
+      'Endpoint para criar um novo carro na base de dados. Acesso restrito a usuários com perfil de "admin".',
+  })
+  // id  Int @id @default(autoincrement())
+  // model String
+  // make String
+  // year Int
+  // colour String
+  // status carStatus
+  // Documentação do Corpo da Requisição (Payload)
+  @ApiBody({
+    type: CreateCarDto,
+    description: "Dados para a criação de um novo carro.",
+    examples: {
+      validRequest: {
+        summary: "Exemplo de requisição válida",
+        value: {
+          make: "Toyota",
+          model: "Corolla",
+          year: 2023,
+          colour: "black",
+          status: "available",
+        },
+      },
+      invalidRequest: {
+        summary: "Exemplo de requisição inválida",
+        description: 'Falta o campo obrigatório "model".',
+        value: {
+          make: "Toyota",
+          year: 2023,
+          colour: "black",
+          status: "available",
+        },
+      },
+    },
+  })
+  // Documentação das Respostas da API
+  @ApiResponse({
+    status: 201,
+    description: "O carro foi criado com sucesso.",
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      "Bad Request: Os dados fornecidos são inválidos ou faltam campos obrigatórios.",
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      "Unauthorized: O token de autenticação não foi fornecido ou é inválido.",
+  })
+  @ApiResponse({
+    status: 403,
+    description:
+      'Forbidden: O usuário não tem permissão para executar esta ação (requer perfil "admin").',
+  })
+  @ApiResponse({
+    status: 404,
+    description: "Not Found: Algum recurso relacionado não foi encontrado.",
+  })
+  @ApiResponse({
+    status: 409,
+    description: "Conflict: Já existe um carro com a mesma placa.",
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      "Internal Server Error: Ocorreu um erro inesperado no servidor.",
+  })
   create(@Body() createCar: CreateCarDto) {
     return this.carsService.create(createCar);
   }
@@ -88,6 +157,7 @@ export class CarsController {
   }
 
   @ApiOperation({ summary: "Deleta um carro do banco de dados." })
+  @ApiBearerAuth()
   @ApiResponse({ status: 200, description: "Carro deletado." })
   @ApiResponse({ status: 403, description: "Não autorizado." })
   @Roles("admin")
--- src/cars/dto/create-car-dto.ts
+++ src/cars/dto/create-car-dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsInt, IsString } from "class-validator";
+import {IsEnum, IsInt, IsNotEmpty, IsString} from "class-validator";
 import { Type } from "class-transformer";
 import { carStatus } from "../enum/carStatus.enum";
 import {ApiProperty} from "@nestjs/swagger";
@@ -6,22 +6,27 @@ import {ApiProperty} from "@nestjs/swagger";
 export class CreateCarDto {
   @ApiProperty({ example: "Astra" })
   @IsString({ message: "O modelo deve ser uma string." })
+  @IsNotEmpty()
   model: string;
 
   @ApiProperty({ example: "Chevrolet" })
   @IsString({ message: "O fabricante deve ser uma string." })
+  @IsNotEmpty()
   make: string;
 
   @ApiProperty({ example: 2007 })
   @IsInt({ message: "O ano deve ser um inteiro." })
+  @IsNotEmpty()
   @Type(() => Number)
   year: number;
 
   @ApiProperty({ example: "Preto" })
+  @IsNotEmpty()
   @IsString({ message: "A cor deve ser uma string." })
   colour: string;
 
   @ApiProperty({ example: "sold" })
+  @IsNotEmpty()
   @IsString({ message: "O status deve ser uma string." })
   @IsEnum(carStatus)
   status: carStatus;

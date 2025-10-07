24e649ce0762f5ad62f82583dc34481ebd893117
Eduardo Falarz <eduardofalarz23@gmail.com>
2025-10-07 18:05:24 -0300

    DOCS: Adding API versioning

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
+import {ValidationPipe, VersioningType} from "@nestjs/common";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
@@ -19,6 +19,12 @@ async function bootstrap() {
     .setVersion("1.0")
     .addBearerAuth()
     .build();
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: "v",
+    defaultVersion: "1",
+  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
